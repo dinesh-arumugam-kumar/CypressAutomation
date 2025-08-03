5e2a52365ea27fccea41eb6a98575cab043aff29
dinesh-arumugam-kumar <dinesh.arumugam.kumar@gmail.com>
2025-08-03 23:57:47 +0530

    Refactor Test1.js to use alias for product locator and improve code readability; add Test2.js for additional test case

--- cypress/integration/examples/Test2.js
+++ cypress/integration/examples/Test2.js
@@ -1,15 +1,12 @@
 ///<reference types="Cypress"/>
 
-describe('My First Test Suite', function(){
-    it('Test 1',function(){
+describe('My Second Test Suite', function(){
+    it('Test 2',function(){
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
         cy.get('.search-keyword').type("ca")
         cy.wait(1000)
-        cy.get('.product:visible').should('have.length',4)
         cy.get('.products').as('ProductLocator') //Aliasing the locator with name
         cy.get('@ProductLocator').find('.product').should('have.length',4)
-        cy.wait(1000)
-        // cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
         cy.get('@ProductLocator').find('.product').each(($el,index, $list) => { // Looping concept
             const textval = $el.find('h4.product-name').text()
             if(textval.includes('Cashews')){
@@ -19,7 +16,9 @@ describe('My First Test Suite', function(){
         cy.get('.brand').should('have.text','GREENKART')
         cy.get('.brand').then(function(logo){ // resolving promises
             cy.log(logo.text()) // Print in test runner
-            console.log(logo.text()) // Print in developer console
         })
+        cy.get('.cart-icon > img').click()
+        cy.contains('PROCEED TO CHECKOUT').click()
+        cy.contains('Place Order').click()
     })
 })
