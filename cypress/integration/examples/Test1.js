d450b9268de7c03b3cdb959f8700bc1c2abeee74
dinesh-arumugam-kumar <dinesh.arumugam.kumar@gmail.com>
2025-08-03 19:25:14 +0530

    Bacis Test

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,5 +1,10 @@
+///<reference types="Cypress"/>
+
 describe('My First Test Suite', function(){
     it('Test 1',function(){
-        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+        cy.get('.search-keyword').type("ca")
+        cy.wait(1000)
+        cy.get('.product:visible').should('have.length',4)
     })
 })
